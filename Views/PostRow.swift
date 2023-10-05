aa8bab00cfbd50f2d44466ca8d9a95cc17889295
Kenneth Rathbun <kenrathbun1@gmail.com>
2023-10-04 23:48:01 -0700

    Updated delete button to prevent entire list row from performing delete action

--- Views/PostRow.swift
+++ Views/PostRow.swift
@@ -34,6 +34,7 @@ struct PostRow: View {
                     Label("Delete", systemImage: "trash")
                 }
                 .labelStyle(.iconOnly)
+                .buttonStyle(.borderless)
             }
         }
         .padding(.vertical)
