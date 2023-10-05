c6733ebe561840c3ca43cffe5844f5f86cdbd6bb
Kenneth Rathbun <kenrathbun1@gmail.com>
2023-10-04 23:56:20 -0700

    Added confirmation dialogue

--- Views/PostRow.swift
+++ Views/PostRow.swift
@@ -13,6 +13,8 @@ struct PostRow: View {
     let post: Post
     let deleteAction: DeleteAction
     
+    @State private var showConfirmationDialog = false
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             HStack {
@@ -30,11 +32,16 @@ struct PostRow: View {
             Text(post.content)
             HStack {
                 Spacer()
-                Button(role: .destructive, action: deletePost) {
+                Button(role: .destructive, action: {
+                    showConfirmationDialog = true
+                }) {
                     Label("Delete", systemImage: "trash")
                 }
                 .labelStyle(.iconOnly)
                 .buttonStyle(.borderless)
+                .confirmationDialog("Are you sure you want to delete this post?", isPresented: $showConfirmationDialog, titleVisibility: .visible) {
+                    Button("Delete", role: .destructive, action: deletePost)
+                }
             }
         }
         .padding(.vertical)
