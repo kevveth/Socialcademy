ce4e04d294c63660e1bbdc880aa85741f5aff54b
Kenneth Rathbun <kenrathbun1@gmail.com>
2023-10-05 17:12:37 -0700

    Added favorite action

--- ViewModels/PostsViewModel.swift
+++ ViewModels/PostsViewModel.swift
@@ -24,10 +24,24 @@ class PostsViewModel: ObservableObject {
         }
     }
     
-    func makeDeleteAction(for post: Post) -> PostRow.DeleteAction {
+    func makeDeleteAction(for post: Post) -> PostRow.Action {
         return { [weak self] in
             try await self?.postsRepository.delete(post)
             self?.posts.value?.removeAll { $0.id == post.id }
+        }
+    }
+    
+    func makeFavoriteAction(for post: Post) -> () async throws -> Void {
+        return { [weak self] in
+            let newValue = !post.isFavorite
+            
+            //Update the document in Firestore
+            try await newValue ? self?.postsRepository.favorite(post) : self?.postsRepository.unfavorite(post)
+            
+            //Update the PostsList
+            guard let index = self?.posts.value?.firstIndex(of: post) else { return }
+            self?.posts.value?[index].isFavorite = newValue
+            
         }
     }
     
