55a6a97b28d68e9b13a4e753899c59a1570f77a2
Kenneth Rathbun <kenrathbun1@gmail.com>
2023-10-05 17:29:09 -0700

    Added favorite button

--- Views/PostRow.swift
+++ Views/PostRow.swift
@@ -33,15 +33,16 @@ struct PostRow: View {
                 .fontWeight(.semibold)
             Text(post.content)
             HStack {
+                FavoriteButton(isFavorite: post.isFavorite, action: favoritePost)
                 Spacer()
                 Button(role: .destructive, action: {
                     showConfirmationDialog = true
                 }) {
                     Label("Delete", systemImage: "trash")
                 }
-                .labelStyle(.iconOnly)
-                .buttonStyle(.borderless)
             }
+            .labelStyle(.iconOnly)
+            .buttonStyle(.borderless)
         }
         .padding(.vertical)
         .confirmationDialog("Are you sure you want to delete this post?", isPresented: $showConfirmationDialog, titleVisibility: .visible) {
@@ -60,6 +61,38 @@ struct PostRow: View {
                 print("[PostRow} Cannot delete post: \(error)")
                 self.error = error
             }
+        }
+    }
+    
+    private func favoritePost() {
+        Task {
+            do {
+                try await favoriteAction()
+            }
+            catch {
+                print("[PostRow] Cannot favorite post: \(error)")
+                self.error = error
+            }
+        }
+    }
+}
+
+private extension PostRow {
+    struct FavoriteButton: View {
+        let isFavorite: Bool
+        let action: () -> Void
+        
+        var body: some View {
+            Button(action: action) {
+                if isFavorite {
+                    Label("Remove from favorites", systemImage: "heart.fill")
+                }
+                else {
+                    Label("Add to favorites", systemImage: "heart")
+                }
+            }
+            .foregroundColor(isFavorite ? .red : .gray)
+            .animation(.default, value: isFavorite)
         }
     }
 }
