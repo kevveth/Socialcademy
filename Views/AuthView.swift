74128f4bbd90da2a59ad3af9866d6c42121eb8cf
Kenneth Rathbun <kenrathbun1@gmail.com>
2023-10-10 20:02:54 -0700

    Updated AuthView to be able to sign in and create account

--- Views/AuthView.swift
+++ Views/AuthView.swift
@@ -15,13 +15,42 @@ struct AuthView: View {
             MainTabView()
         }
         else {
+            NavigationView {
+                SignInForm(viewModel: viewModel.makeSignInViewModel()) {
+                    NavigationLink("Create Account", destination: CreateAccountForm(viewModel: viewModel.makeCreateAccountViewModel()))
+                }
+            }
+        }
+    }
+}
+
+private extension AuthView {
+    struct CreateAccountForm: View {
+        @StateObject var viewModel: AuthViewModel.CreateAccountViewModel
+        
+        var body: some View {
+            Form {
+                TextField("Name", text: $viewModel.name)
+                TextField("Email", text: $viewModel.email)
+                SecureField("Password", text: $viewModel.password)
+                Button("Create Account", action: viewModel.submit)
+            }
+            .navigationTitle("Create Account")
+        }
+    }
+    
+    struct SignInForm<Footer: View>: View{
+        @StateObject var viewModel: AuthViewModel.SignInViewModel
+        @ViewBuilder let footer: () -> Footer
+        
+        var body: some View {
             Form {
                 TextField("Email", text: $viewModel.email)
                 SecureField("Password", text: $viewModel.password)
-                Button("Sign Up") {
-                    viewModel.signIn()
-                }
+                Button("Sign In", action: viewModel.submit)
+                footer()
             }
+            .navigationTitle("Sign In")
         }
     }
 }
