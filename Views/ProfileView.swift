249e9d6494f57f4aec6645c1576daed2636df6dc
Kenneth Rathbun <kenrathbun1@gmail.com>
2023-10-10 18:23:29 -0700

    Added a way to sign out

---
+++ Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Socialcademy
+//
+//  Created by Kenneth Oliver Rathbun on 10/10/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
