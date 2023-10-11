4c11ad058d52113f31438ad9ca210b7916447108
Kenneth Rathbun <kenrathbun1@gmail.com>
2023-10-10 19:48:48 -0700

    Subclassed the FormViewModel for use in the SignInForm and CreateAccountForm

--- ViewModels/AuthViewModel.swift
+++ ViewModels/AuthViewModel.swift
@@ -13,21 +13,31 @@ class AuthViewModel: ObservableObject {
     
     private let authService = AuthService()
     
-    @Published var email = ""
-    @Published var password = ""
-    
     init() {
         authService.$isAuthenticated.assign(to: &$isAuthenticated)
     }
     
-    func signIn() {
-        Task {
-            do {
-                try await authService.signIn(email: email, password: password)
-            }
-            catch {
-                print("[AuthViewModel] \(error)")
-            }
+    func makeSignInViewModel() -> SignInViewModel {
+        return SignInViewModel(action: authService.signIn(email:password:))
+    }
+    
+    func makeCreateAccountViewModel() -> CreateAccountViewModel {
+        return CreateAccountViewModel(action: authService.createAccount(name:email:password:))
+    }
+}
+
+
+extension AuthViewModel {
+    class SignInViewModel: FormViewModel<(email: String, password: String)> {
+        convenience init(action: @escaping Action) {
+            self.init(initialValue: (email: "", password: ""), action: action)
+        }
+    }
+    
+    class CreateAccountViewModel: FormViewModel<(name: String, email: String, password: String)>
+    {
+        convenience init(action: @escaping Action) {
+            self.init(initialValue: (name: "", email: "", password: ""), action: action)
         }
     }
 }
