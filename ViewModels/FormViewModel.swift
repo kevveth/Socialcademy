f1aaa030526f7cf16ae917b874db2b595afe7aa0
Kenneth Rathbun <kenrathbun1@gmail.com>
2023-10-10 19:30:06 -0700

    Built the form view model

---
+++ ViewModels/FormViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  FormViewModel.swift
+//  Socialcademy
+//
+//  Created by Kenneth Oliver Rathbun on 10/10/23.
+//
+
+import Foundation
+
+@MainActor
+@dynamicMemberLookup
+class FormViewModel<Value>: ObservableObject {
+    typealias Action = (Value) async throws -> Void
+    
+    @Published var value: Value
+    @Published var error: Error?
+    
+    subscript<T>(dynamicMember keyPath: WritableKeyPath<Value, T>) -> T {
+        get { value[keyPath: keyPath] }
+        set { value[keyPath: keyPath] = newValue }
+    }
+    
+    private let action: Action
+    
+    init(initialValue: Value, action: @escaping Action) {
+        self.value = initialValue
+        self.action = action
+    }
+    
+    private func handleSubmit() async
+    {
+        do {
+            try await action(value)
+        }
+        catch {
+            print("[FormViewModel] Cannot submit: \(error)")
+            self.error = error
+        }
+    }
+    
+    nonisolated func submit() {
+        Task {
+            await handleSubmit()
+        }
+    }
+}
