03073bead2a3aa1a53cb6c072bf0467f46d06718
Kenneth Rathbun <kenrathbun1@gmail.com>
2023-10-10 18:30:47 -0700

    Added a way to sign out (pt. 2)

--- Views/ProfileView.swift
+++ Views/ProfileView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Sign Out") {
+            try! Auth.auth().signOut()
+        }
     }
 }
 
